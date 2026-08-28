310bb75daef93bd379eaec1b7df3bf7dfc605177
Stefan Samson <ss42701@outlook.com>
2026-08-28 16:03:53 +0300

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -184,7 +184,7 @@ export default defineConfig({
       provider: fontSource,
       name: "Inter",
       cssVariable: "--font-inter",
-      weights: [400, 500, 800],
+      weights: [300, 400, 500, 800],
       subsets: ["latin"]
     },
     {
